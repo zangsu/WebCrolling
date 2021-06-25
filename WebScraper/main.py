f9e27294038970cc82f1ca9e0879054d44305c78
zangsu <76612738+zangsu@users.noreply.github.com>
2021-06-25 23:41:59 +0900

    Add files via upload

--- WebScraper/main.py
+++ WebScraper/main.py
@@ -4,4 +4,7 @@ from page import find_page, extract_notice
 last_page = find_page()
 
 notice = extract_notice(last_page)
-print(notice)
+#print(notice)
+for element in notice:
+    print(f"제목 : {element[0]}, 작성자 : {element[1]}")  
+    
